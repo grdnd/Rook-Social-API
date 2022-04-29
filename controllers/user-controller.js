6b7ce599ac64891412d3d5a0ee51eb96d839bf24
Gordo <gordonhdo@gmail.com>
2022-04-29 08:06:13 -0700

    created user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,34 +1,114 @@
 // Require Models
-const res = require('express/lib/response');
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 const userController = {
   // get all users
-  // get user by ID
+  getUsers(req, res) {
+    User.find()
+    .select('-__v')
+    .then((UsersData) => {
+      res.json(UsersData)
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  },
+  // get user by id
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.userId })
+    .select('-__v')
+    .populate('friends')
+    .populate('thoughts')
+    .then((user) => {
+      if (user == null) {
+        return res.status(404).json({ 'User id was not found' });
+      }
+      res.json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+  },
   // create new user
+  newUser(req, res) {
+    User.create(req.body)
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  },
   // update user by ID
+  editUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      {
+        $set: req.body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((user) => {
+        if (user == null) {
+          return res.status(404).json({ message: "User id was not found" });
+        }
+        res.json(user);
+      })
+      .catch ((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
   // delete user by ID
-  //      BONUS: delete associated thoughts
-  //      BONUS: get id of user's 'thoughts' and delete them all
+  delUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+    .then((user) => {
+      if (user == null) {
+        return res.status(404).json({ message: 'User id was not found' });
+      }
+      //      BONUS: delete associated thoughts
+      return Thought.deleteMany({ _id: { $in: user.thoughts } });
+    })
+    .then(() => {
+      res.json({ message: 'Users have been deleted, along with their minds'})
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+  },
   // add friend to friend list
+  addFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true })
+    .then((user) => {
+      if (user == null) {
+        return res.status(404).json({ message: 'No user found with this id' });
+      }
+      res.json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  },
   // remove friend from friend list by ID
-}
-
-async function getUser(req, res, next) {
-  // create a user
-  let user;
-  try {
-    // find user by ID
-    user = await User.findById(req.params.id);
-    // check if user exists
-    if(user == null) {
-      return res.status(404).json({ message: "Cannot find user" });
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-  res.user = user;
-  next();
+  removeFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true })
+    .then ((user) => {
+      if (user == null) {
+        return res.status(404).json({ message: 'No user found with this id' });
+      }
+      res.json(user);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+  },
 };
 
 module.exports = userController;
