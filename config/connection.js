0861b8e80c90271f9bb72f21f96889f8fd9d3fbd
Gordo <gordonhdo@gmail.com>
2022-04-29 06:14:20 -0700

    adding connections to db / environment files

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+// Node Modules
+// Require Mongoose
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+module.exports = mongoose.connection;
