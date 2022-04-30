d4008fba07009cb6eb82fda61abc666e74a2f754
Gordo <gordonhdo@gmail.com>
2022-04-29 21:24:34 -0700

    adding Thought models to model index

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 // require user model schema
 const User = require('./User');
+// require thought model schema
+const Thought = require('./Thought');
 
-module.exports = { User };
+module.exports = { User, Thought };
